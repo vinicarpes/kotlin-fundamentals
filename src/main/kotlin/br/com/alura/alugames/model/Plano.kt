80c4f1cc7c6cf3fbe51986478da81e99211b48af
vinicarpes <vinicdf000@gmail.com>
2025-08-15 14:12:12 -0300

    refcator: make Plano a sealed abstract class

--- src/main/kotlin/br/com/alura/alugames/model/Plano.kt
+++ src/main/kotlin/br/com/alura/alugames/model/Plano.kt
@@ -1,6 +1,6 @@
 package org.example.br.com.alura.alugames.model
 
-abstract class Plano(tipo : String) {
+sealed class Plano(tipo : String) {
     open fun obterValor(aluguel: Aluguel): Double{
         return aluguel.periodo.emDias * aluguel.jogo.preco
     }
